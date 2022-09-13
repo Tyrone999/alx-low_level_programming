e970f19d8d7ac0a3bdf83c3d66681335d514571e
Tyrone999 <tyronepietersreece@gmail.com>
2022-09-13 03:47:07 -0700

    0._putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints putchar
+ *
+ * Description: prints putchar while returning 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return(0);
+}
