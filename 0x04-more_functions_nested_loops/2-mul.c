b60f297863f25d380d5d4625461d954e2f3447c0
Tyrone999 <tyronepietersreece@gmail.com>
2022-09-15 03:50:05 -0700

    added multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplies values provided
+ * @a: first value
+ * @b: second value
+ * Return: returns answer
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
