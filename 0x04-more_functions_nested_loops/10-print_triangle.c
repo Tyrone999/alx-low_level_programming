a0044dff9da842a7443d2f911b9e14bc3858d7d9
Tyrone999 <tyronepietersreece@gmail.com>
2022-09-15 15:02:11 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - preints a triangle
+ */
+void print_triangle(int size)
+{
+	int spaces;
+	int hashes;
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		spaces = size - i - 1;
+		hashes = i + 1;
+		while (spaces > 0)
+		{
+			_putchar(' ');
+			spaces--;
+		}
+		while (hashes > 0)
+		{
+			_putchar('#');
+			hashes--;
+		}
+		_putchar('\n');
+	}
+	if(size <= 0)
+		_putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,3 +9,4 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 int _putchara(char *c);
+void print_triangle(int size);
