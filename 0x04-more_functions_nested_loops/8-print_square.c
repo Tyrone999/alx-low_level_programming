add3ba4e054f0cae4f420136741db6ae95c88890
Tyrone999 <tyronepietersreece@gmail.com>
2022-09-15 13:39:08 -0700

    8-squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - prionts a square
+ * @size: size of the square
+ * Return: Always 0.
+ */
+void print_square(int size)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < size; x++)
+	{
+		for (y = 0; y < size; y++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (size == 0)
+		_putchar('\n');
+}
