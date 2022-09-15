b9cd006079865ccebbf37668c55f7b138b092e26
Tyrone999 <tyronepietersreece@gmail.com>
2022-09-15 12:52:01 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - diagonal line
+ *
+ * @n: input
+ *
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int spaces;
+
+	for (i = 0; i < n; i++)
+	{
+		for (spaces = 0; spaces < i; spaces++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
