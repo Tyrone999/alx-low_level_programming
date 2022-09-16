43d73a434128b541bc2863db4b9565fc8c6917d7
Tyrone999 <tyronepietersreece@gmail.com>
2022-09-16 13:45:27 -0700

    fixed betty issues

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -11,3 +14,5 @@ void print_square(int size);
 int _putchara(char *c);
 void print_triangle(int size);
 void print_number(int n);
+
+#endif
