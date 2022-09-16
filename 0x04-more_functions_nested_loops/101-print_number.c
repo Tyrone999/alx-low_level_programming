e74bad13c4601e20c0485bf04ac26d6749af332c
Tyrone999 <tyronepietersreece@gmail.com>
2022-09-16 13:38:21 -0700

    Using _putchar to print entire int

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_number - Uses a recursive way to print entire provided int
+ * @n; n is the provided int
+ */
+void print_number(int n)
+{
+	/* Check if negative and set positive*/
+	if (n < 0)
+	{
+		_putchar('-');
+		n = n * -1;
+	}
+	/* Uses own function to print previous numbers using _putchar*/
+	if((n / 10) != 0)
+		print_number(n / 10);
+	_putchar(n % 10 + '0');
+
+}
