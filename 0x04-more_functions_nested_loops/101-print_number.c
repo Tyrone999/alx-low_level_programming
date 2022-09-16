aa99244cb49123c953b081358a7e6b3c0cbff076
Tyrone999 <tyronepietersreece@gmail.com>
2022-09-16 13:50:08 -0700

    Why is check 12 wrong ...

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,14 +6,12 @@
  */
 void print_number(int n)
 {
-	/* Check if negative and set positive*/
 	if (n < 0)
 	{
 		_putchar('-');
 		n = n * -1;
 	}
-	/* Uses own function to print previous numbers using _putchar*/
-	if ((n / 10) != 0)
+	if ((n / 10) <= 0)
 	{
 		print_number(n / 10);
 	}
