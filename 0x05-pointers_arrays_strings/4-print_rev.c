759f00a91d0ce7c36e81992a3d3743673b18f967
Tyrone999 <tyronepietersreece@gmail.com>
2022-09-19 13:57:50 -0700

    4-preintreversed

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - prints the provided string in reverse
+ * @s: The provided string
+ */
+
+void print_rev(char *s)
+{
+	int counter;
+
+	counter = 0;
+	while (s[counter])
+	{		
+		counter++;
+	}
+	i--;
+	while (i >= 0)
+	{
+		putchar(s[i])
+		i--;
+	}
+	putchar('\n');
+}
